0991fba1c161dce70209f11bb8c2bace3d908196
mo111-w2 <momennassar25@gmail.com>
2025-12-19 01:00:53 +0200

    Update main.py

--- entery_point/main.py
+++ entery_point/main.py
@@ -1,14 +1,41 @@
-#main.py
-#entry point for the project
+import sys
+import os
 
 
-from core.node import Node
-from core.utlis import manhattan, euclidean, is_valid, get_neighbors
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
+from search_algorithms.uninformed_search.bfs import bfs
+from search_algorithms.uninformed_search.dfs import dfs
 
-#ready for import search algorithms implementation in other files
 
 if __name__ == "__main__":
-    print("Program entry point ready,Algorithms will be called  after implementation")
 
+    grid = [
+        [1, 1, 1, 1],
+        [1, 0, 1, 0],
+        [1, 1, 1, 1],
+        [0, 1, 1, 1]
+    ]
 
+    start = (0, 0)
+    goal = (2, 3)
+
+   
+    bfs_path = bfs(grid, start, goal)
+    if bfs_path:
+        print("BFS Path Found:")
+        print(bfs_path)
+        print("BFS Path length:", len(bfs_path) - 1)
+    else:
+        print("BFS: No path found")
+
+    print("---------------------------------------")
+
+    
+    dfs_path = dfs(grid, start, goal)
+    if dfs_path:
+        print("DFS Path Found:")
+        print(dfs_path)
+        print("DFS Path length:", len(dfs_path) - 1)
+    else:
+        print("DFS: No path found")
